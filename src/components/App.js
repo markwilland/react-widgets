02d5a9b0dcfaa235af63cd8ce88ffbae306ebe68
markwilland <markwillanderson@gmail.com>
2021-04-18 23:59:03 -0400

    created accordion with function based use state

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end Javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a fav library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use react by creating components'
+    }
+];
+
+const App = props => {
+        return (
+            <div>
+                <br />
+                <Accordion items={items}/>
+            </div>
+        );    
+}
+
+export default App;
