31715e4cf9c13994ee28ed19175a565f4b234685
markwilland <markwillanderson@gmail.com>
2021-04-23 22:59:55 -0400

    added debounce + processed html response from api request

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,28 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Html5Entities } from 'html-entities';
 
 function Convert({language, text}) {
     const [translation, setTranslation] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+    const htmlEntities = new Html5Entities();
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text])
 
     useEffect(() => {
         const doTranslation = async () => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-                },
+                }
             }
         );
             setTranslation(data.data.translations[0].translatedText);
         }
 
         doTranslation();
-    }, [language, text])
+    }, [language, debouncedText])
 
     return (
         <div>
-            <h1 className="ui header">{translation}</h1>
+            <h1 className="ui header">{htmlEntities.decode(translation)}</h1>
         </div>
     )
 }
